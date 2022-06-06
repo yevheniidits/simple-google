95d9959782ddbbea5108e8603a0604bfe93ebb6c
yevheniidits <yevheniidits@gmailcom>
2022-06-06 10:22:33 +0300

    init commit

---
+++ setup.py
@@ -0,0 +1,19 @@
+__author__ = 'Yevhenii Dits'
+
+import setuptools
+
+
+setuptools.setup(
+    name='simple-google-services',
+    version='0.0.1',
+    license='MIT',
+    author='Yevhenii Dits',
+    author_email='yevheniidits@gmail.com',
+    description='Simplifies interaction with Google API in the most frequently used tasks.',
+    packages=setuptools.find_packages(),
+    zip_safe=False,
+    install_requires=(
+        'google-api-python-client==2.49.0',
+        'google-auth-oauthlib==0.5.1'
+    )
+)
