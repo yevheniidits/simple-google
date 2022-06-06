f67bebf054b7efd01db300adeb13e696e3e1433d
yevheniidits <yevheniidits@gmailcom>
2022-06-06 19:51:42 +0300

    add add_reply method; reformat

--- sgs/apps/services/youtube_services.py
+++ sgs/apps/services/youtube_services.py
@@ -16,10 +16,46 @@ class YoutubeService:
 
     @property
     def service(self):
+        """
+        Create Resource object for interacting with Youtube API
+        """
         if not self._service:
             account = Account(config.client_secret_file, config.scopes)
             self._service = GoogleServices(account).youtube()
         return self._service
+
+    @staticmethod
+    def _save_comment(comment: Union['Comment', 'Reply'], api_response: dict):
+        """
+        Parse API data and update selected Comment/Reply and CommentAuthor instances.
+        :param comment: Comment or Reply instance
+        :param api_response: response from API
+        """
+        if api_response['kind'] == 'youtube#commentThread':
+            api_response = api_response['snippet']['topLevelComment']
+        comment.item_id = api_response['id']
+        comment.text = api_response['snippet']['textOriginal']
+        comment.likes = api_response['snippet']['likeCount']
+        comment.published = api_response['snippet']['publishedAt']
+        comment.updated = api_response['snippet']['updatedAt']
+        comment.author = CommentAuthor(comment)
+        comment.author.name = api_response['snippet']['authorDisplayName']
+        comment.author.image_url = api_response['snippet']['authorProfileImageUrl']
+        comment.author.channel_url = api_response['snippet']['authorChannelUrl']
+        return
+
+    @staticmethod
+    def _save_video(video: 'Video', api_response: dict):
+        """
+        Parse API data and update selected Video instance.
+        :param video: Video instance
+        :param api_response: response from API
+        """
+        video.item_id = api_response['snippet']['resourceId']['videoId']
+        video.title = api_response['snippet']['title']
+        video.description = api_response['snippet']['description']
+        video.published = api_response['snippet']['publishedAt']
+        return
 
 
 class Channel(YoutubeService):
@@ -27,6 +63,7 @@ class Channel(YoutubeService):
     Subclass that contains data about authenticated user`s channel.
     NOT FOR DIRECT USING. Use Youtube class instead.
     """
+
     __slots__ = ('item_id', 'playlist_id', '_my_videos')
 
     def __init__(self, channel_id: str):
@@ -43,23 +80,20 @@ class Channel(YoutubeService):
         """
         Retrieve the list of videos uploaded to the authenticated user's channel.
         """
-        items_list_request = self.service.playlistItems().list(
+        api_request = self.service.playlistItems().list(
             playlistId=self.playlist_id,
             part='snippet',
             maxResults=5
         )
-        while items_list_request:
-            items_list_response = items_list_request.execute()
+        while api_request:
+            api_response = api_request.execute()
             # Save information about each uploaded video.
-            for uploaded_video in items_list_response['items']:
+            for video_api_response in api_response['items']:
                 video = Video(self)
-                video.item_id = uploaded_video['snippet']['resourceId']['videoId']
-                video.title = uploaded_video['snippet']['title']
-                video.description = uploaded_video['snippet']['description']
-                video.published = uploaded_video['snippet']['publishedAt']
+                self._save_video(video, video_api_response)
                 self._my_videos.append(video)
             # Check if more videos are exists
-            items_list_request = self.service.playlistItems().list_next(items_list_request, items_list_response)
+            api_request = self.service.playlistItems().list_next(api_request, api_response)
         return self._my_videos
 
     @property
@@ -75,6 +109,7 @@ class Video(YoutubeService):
     Subclass that contains data about authenticated user`s uploaded video.
     NOT FOR DIRECT USING. Use Youtube class instead.
     """
+
     __slots__ = ('channel', 'item_id', 'title', 'description', 'published', '_comments')
 
     def __init__(self, channel: Channel):
@@ -99,18 +134,9 @@ class Video(YoutubeService):
             videoId=self.item_id,
             textFormat='plainText'
         ).execute()['items']
-        for video_comment_data in comments_response:
-            comment_data = video_comment_data['snippet']['topLevelComment']['snippet']
+        for response_data in comments_response:
             comment = Comment(self)
-            comment.item_id = video_comment_data['snippet']['topLevelComment']['id']
-            comment.published = comment_data['publishedAt']
-            comment.updated = comment_data['updatedAt']
-            comment.likes = comment_data['likeCount']
-            comment.text = comment_data['textOriginal']
-            comment.author = CommentAuthor(comment)
-            comment.author.name = comment_data['authorDisplayName']
-            comment.author.image_url = comment_data['authorProfileImageUrl']
-            comment.author.channel_url = comment_data['authorChannelUrl']
+            self._save_comment(comment, response_data)
             self._comments.append(comment)
         return self._comments
 
@@ -127,24 +153,25 @@ class Comment(YoutubeService):
     Subclass that contains data about comment for uploaded video.
     NOT FOR DIRECT USING. Use Youtube class instead.
     """
-    __slots__ = ('video', 'item_id', '_author', 'text', 'likes', 'published', 'updated', '_replies')
+
+    __slots__ = ('video', 'item_id', 'author', 'text', 'likes', 'published', 'updated', '_replies')
 
     def __init__(self, video: Video):
         super().__init__()
         self.video = video
-        self.item_id: str = ''
-        self._author = None
-        self.text: str = ''
-        self.likes: int = 0
-        self.published: str = ''
-        self.updated: str = ''
-        self._replies: list = []
+        self.item_id = None
+        self.author = None
+        self.text = None
+        self.likes = None
+        self.published = None
+        self.updated = None
+        self._replies = []
 
     def __str__(self):
         return self.text
 
     @property
-    def replies(self) -> list:
+    def replies(self):
         """
         Retrieve the list of replies for comment.
         """
@@ -155,31 +182,30 @@ class Comment(YoutubeService):
         ).execute()['items']
         for reply_data in replies_response:
             reply = Reply(self)
-            reply.text = reply_data['snippet']['textOriginal']
-            reply.likes = reply_data['snippet']['likeCount']
-            reply.published = reply_data['snippet']['publishedAt']
-            reply.updated = reply_data['snippet']['updatedAt']
-            reply.author = CommentAuthor(reply)
-            reply.author.name = reply_data['snippet']['authorDisplayName']
-            reply.author.image_url = reply_data['snippet']['authorProfileImageUrl']
-            reply.author.channel_url = reply_data['snippet']['authorChannelUrl']
+            self._save_comment(reply, reply_data)
             self._replies.append(reply)
         return self._replies
 
     @property
-    def replies_count(self) -> int:
+    def replies_count(self):
         """
         Total number of replies for comment.
         """
         return len(self.replies)
 
-    @property
-    def author(self):
-        return self._author
-
-    @author.setter
-    def author(self, author):
-        self._author = author
+    def add_reply(self, text: str):
+        reply_data = self.service.comments().insert(
+            part="snippet",
+            body=dict(
+                snippet=dict(
+                    parentId=self.item_id,
+                    textOriginal=text
+                )
+            )
+        ).execute()
+        reply = Reply(self)
+        self._save_comment(reply, reply_data)
+        return reply
 
 
 class Reply(YoutubeService):
@@ -188,27 +214,20 @@ class Reply(YoutubeService):
     NOT FOR DIRECT USING. Use Youtube class instead.
     """
 
-    __slots__ = ('comment', '_author', 'text', 'likes', 'published', 'updated')
+    __slots__ = ('comment', 'item_id', 'author', 'text', 'likes', 'published', 'updated')
 
     def __init__(self, comment: Comment):
         super().__init__()
         self.comment = comment
-        self._author = None
-        self.text: str = ''
-        self.likes: int = 0
-        self.published: str = ''
-        self.updated: str = ''
+        self.item_id = None
+        self.author = None
+        self.text = None
+        self.likes = None
+        self.published = None
+        self.updated = None
 
     def __str__(self):
         return self.text
-
-    @property
-    def author(self):
-        return self._author
-
-    @author.setter
-    def author(self, author):
-        self._author = author
 
 
 class CommentAuthor(YoutubeService):
